5edaa9951777f60350806365f0a4a2bc2223d136
hab <Mohabzax4@gmail.com>
2023-02-22 10:59:35 +0200

    Navigation Animation

--- Ludos/Assets/Scripts/MainApp/Navigation.cs
+++ Ludos/Assets/Scripts/MainApp/Navigation.cs
@@ -5,49 +5,60 @@ using UnityEngine.SceneManagement;
 
 public class Navigation : MonoBehaviour
 {
+    public Animator animator;
+
+    IEnumerator loadScene(int index)
+    {
+        animator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene(index);
+    }
+
     /// <MainPages>
     public void NavToHomePage()
     {
-        SceneManager.LoadScene(15);
+        StartCoroutine(loadScene(15));
     }
 
     public void NavToGamePage()
     {
-        SceneManager.LoadScene(14);
+        StartCoroutine(loadScene(14));
     }
 
-    public void NavToSettingsPage()
-    {
-        SceneManager.LoadScene(15);
-    }
+    //public void NavToSettingsPage()
+    //{
+    //    SceneManager.LoadScene(15);
+    //}
 
-    public void NavToAchivments()
-    {
-        //SceneManager.LoadScene();
-    }
+    //public void NavToAchivments()
+    //{
+    //    //SceneManager.LoadScene();
+    //}
 
-    public void NavToCreaditis()
-    {
-        //SceneManager.LoadScene();
-    }
+    //public void NavToCreaditis()
+    //{
+    //    //SceneManager.LoadScene();
+    //}
     /// </MainPages>
 
     ///<Games>
 
-    public void NavToMathTower()
-    {
-        SceneManager.LoadScene(0);
-    }
+    //public void NavToMathTower()
+    //{
+    //    SceneManager.LoadScene(0);
+    //}
 
-    public void NavToAnimals()
-    {
-        SceneManager.LoadScene(7);
-    }
+    //public void NavToAnimals()
+    //{
+    //    SceneManager.LoadScene(7);
+    //}
 
-    public void NavToDatesAndTime()
-    {
-        //SceneManager.LoadScene(0);
-    }
+    //public void NavToDatesAndTime()
+    //{
+    //    //SceneManager.LoadScene(0);
+    //}
 
     ///</Games>
 
