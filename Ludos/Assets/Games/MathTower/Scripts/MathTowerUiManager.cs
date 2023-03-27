46fa4b285fc015637f5df4bde9619d0fa42b4e45
hab <Mohabzax4@gmail.com>
2023-03-27 13:35:28 +0200

    prefix Video
    #34 replace the video scenes and with video start before the level begin

--- Ludos/Assets/Games/MathTower/Scripts/MathTowerUiManager.cs
+++ Ludos/Assets/Games/MathTower/Scripts/MathTowerUiManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class MathTowerUiManager : MonoBehaviour
 {
@@ -13,22 +14,37 @@ public class MathTowerUiManager : MonoBehaviour
     [SerializeField] Image _starsImg;
     [SerializeField] public Image congrates;
     [SerializeField] public RawImage confetti;
+    [SerializeField] public RawImage _video;
     string[] _questionText;
+    // check if the level end for updating the stars
+    public bool isActive = false;
+    private bool isFinished = false;
     Scene scene;
+    [SerializeField]Timer timer;
     // Start is called before the first frame update
     void Start()
     {
         scene = SceneManager.GetActiveScene();
         getQuestion();
-
+        _video.GetComponent<VideoPlayer>().loopPointReached += EndReached;
         _question.text = _questionText[0];
-        DesableObjects();
+        congrates.gameObject.SetActive(false);
+        timer.SetDuration(60).Begin();
+        Timer.SetPaused(true);
+    }
+    private void Update()
+    {
+        if (isFinished)
+        {
+            _video.enabled = false;
+            Timer.SetPaused(false);
+        }
     }
 
-    private void DesableObjects()
+
+    void EndReached(VideoPlayer vp)
     {
-        congrates.gameObject.SetActive(false);
-        confetti.enabled = false;
+        isFinished = true;
     }
 
     private void getQuestion()
@@ -87,5 +103,20 @@ public class MathTowerUiManager : MonoBehaviour
             }
         }
     }
-
+    public IEnumerator FinishingLevel()
+    {
+        yield return new WaitForSeconds(0.7f);
+        // the white rect that contain the question
+        textBackground.gameObject.SetActive(false);
+        Timer.SetPaused(true);
+        yield return new WaitForSeconds(0.3f);
+        // finishing level Screen
+        congrates.gameObject.SetActive(true);
+        UpdateStars(Timer.second, isActive);
+        isActive = true;
+        // confetti is the finishing video
+        confetti.enabled = true;
+        // if the level finished
+        MathTowerGameManager.instance.isCompleted = true;
+    }
 }
