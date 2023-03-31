65020ca61657bdc7a38055162bcb99808cdbdd5f
hab <Mohabzax4@gmail.com>
2023-03-31 19:51:57 +0200

    Integrate with **GameManger**

---
+++ Ludos/Assets/Games/Animals/Scripts/AnimalsMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalsMenu : MonoBehaviour
+{
+    public Button[] LevelButtons;
+    [SerializeField] Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateLevels();
+    }
+
+    void UpdateLevels()
+    {
+        for (int i = 0; i < GameManager.instance.animalsNextLevel; i++)
+        {
+            LevelButtons[i].interactable = true;
+        }
+        slider.value = GameManager.instance.animalsNextLevel;
+    }
+}
