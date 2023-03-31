ba65090febc1f9265b887f54bff81600aa92b95a
hab <Mohabzax4@gmail.com>
2023-03-31 17:02:23 +0200

    integrate with `GameManager`

---
+++ Ludos/Assets/Games/MathTower/Scripts/MathTowerMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MathTowerMenu : MonoBehaviour
+{
+    public Button[] LevelButtons;
+    [SerializeField] Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateLevels();
+    }
+
+    void UpdateLevels()
+    {
+        for (int i = 0; i < GameManager.instance.mathTowerNextLevel; i++)
+        {
+            LevelButtons[i].interactable = true;
+        }
+        slider.value = GameManager.instance.mathTowerNextLevel;
+    }
+}
